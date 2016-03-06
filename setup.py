8502b8e1cdafaa850249558d0cebe3f0eb77edb5
Jonathan Slenders <jonathan@slenders.be>
2016-03-06 15:28:59 +0100

    Release 0.8.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = open(
 setup(
     name='pymux',
     author='Jonathan Slenders',
-    version='0.7',
+    version='0.8',
     license='LICENSE',
     url='https://github.com/jonathanslenders/',
     description='Pure Python terminal multiplexer.',
